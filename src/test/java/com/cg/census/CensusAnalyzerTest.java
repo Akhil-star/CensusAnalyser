07532aa7229b43b477aa1259c909be09da610a98
Akhil-star <akhilakki1305@gmail.com>
2020-10-27 18:51:48 +0530

    Wrong file path

--- src/test/java/com/cg/census/CensusAnalyzerTest.java
+++ src/test/java/com/cg/census/CensusAnalyzerTest.java
@@ -11,17 +11,15 @@ public class CensusAnalyzerTest {
     private static final String STATE_CENSUS_CSV_FILE_PATH_WITH_WRONG_HEADER = "StateCensusDataWrongHeader.csv";
     private static final String STATE_CENSUS_CSV_FILE_PATH_WITH_WRONG_DELIMETER = "StateCensusDataWrongDelimiter.csv";
     private static final String STATE_CODE_CSV_FILE_PATH = "StateCode.csv";
-    private static final String STATE_CODE_CSV_FILE_PATH_WITH_WRONG_HEADER = "StateCodeWrongHeader.csv";
-    private static final String STATE_CODE_CSV_FILE_PATH_WITH_WRONG_DELIMETRE = "StateCodeWrongDelimiter.csv";
 
     @Test
     public void givenStateCensusCSVFileShouldReturnNumberOfCorrectRecords() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadCensusData(STATE_CENSUS_CSV_FILE_PATH);
-            Assert.assertEquals(29, numOfRecords);
+            int numOfRecords = censusAnalyser.loadCensusData( STATE_CENSUS_CSV_FILE_PATH );
+            Assert.assertEquals( 29, numOfRecords );
         } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
+            System.out.println( e.getMessage() );
         }
     }
 
@@ -29,20 +27,21 @@ public class CensusAnalyzerTest {
     public void givenStateCensusDataWithWrongFileShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadCensusData(WRONG_CSV_FILE_PATH);
+            censusAnalyser.loadCensusData( WRONG_CSV_FILE_PATH );
         } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);        }
+            System.out.println( e.getMessage() );
+            Assert.assertEquals( CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type );
+        }
     }
 
     @Test
     public void givenStateCensusDataWithWrongFileTypeShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadCensusData(WRONG_FILE_TYPE);
+            censusAnalyser.loadCensusData( WRONG_FILE_TYPE );
         } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+            System.out.println( e.getMessage() );
+            Assert.assertEquals( CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type );
         }
     }
 
@@ -50,10 +49,10 @@ public class CensusAnalyzerTest {
     public void givenIndiaCensusDataWithIncorrectDelimiterShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadCensusData(STATE_CENSUS_CSV_FILE_PATH_WITH_WRONG_DELIMETER);
+            censusAnalyser.loadCensusData( STATE_CENSUS_CSV_FILE_PATH_WITH_WRONG_DELIMETER );
         } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.RUN_TIME_EXCEPTION, e.type);
+            System.out.println( e.getMessage() );
+            Assert.assertEquals( CensusAnalyserException.ExceptionType.RUN_TIME_EXCEPTION, e.type );
         }
     }
 
@@ -61,10 +60,10 @@ public class CensusAnalyzerTest {
     public void givenStateCensusDataWithWrongHeaderShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadCensusData(STATE_CENSUS_CSV_FILE_PATH_WITH_WRONG_HEADER);
+            censusAnalyser.loadCensusData( STATE_CENSUS_CSV_FILE_PATH_WITH_WRONG_HEADER );
         } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-           Assert.assertEquals(CensusAnalyserException.ExceptionType.RUN_TIME_EXCEPTION, e.type);
+            System.out.println( e.getMessage() );
+            Assert.assertEquals( CensusAnalyserException.ExceptionType.RUN_TIME_EXCEPTION, e.type );
         }
     }
 
@@ -73,10 +72,10 @@ public class CensusAnalyzerTest {
     public void givenIndianStateCodeFile_ShouldReturn_CorrectRecords() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadStateCode(STATE_CODE_CSV_FILE_PATH);
-            Assert.assertEquals(37, numOfRecords);
+            int numOfRecords = censusAnalyser.loadStateCode( STATE_CODE_CSV_FILE_PATH );
+            Assert.assertEquals( 37, numOfRecords );
         } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
+            System.out.println( e.getMessage() );
         }
     }
 
@@ -84,45 +83,13 @@ public class CensusAnalyzerTest {
     public void givenStateCode_WithWrongFile_ShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadStateCode(WRONG_CSV_FILE_PATH);
+            censusAnalyser.loadStateCode( WRONG_CSV_FILE_PATH );
         } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+            System.out.println( e.getMessage() );
+            Assert.assertEquals( CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type );
         }
     }
 
-    @Test
-    public void givenStateCodeFile_WithWrongFileType_ShouldThrowException() {
-        try {
-            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadStateCode(WRONG_FILE_TYPE);
-        } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
-        }
-    }
-
-    @Test
-    public void givenStateCodeCSVFile_WithIncorrectDelimiter_ShouldThrowException() {
-        try {
-            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadStateCode(STATE_CODE_CSV_FILE_PATH_WITH_WRONG_DELIMETRE);
-        } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.RUN_TIME_EXCEPTION, e.type);
-        }
-    }
-
-    @Test
-    public void givenStateCodeCSVFile_WithWrongHeader_ShouldThrowException() {
-        try {
-            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            censusAnalyser.loadStateCode(STATE_CODE_CSV_FILE_PATH_WITH_WRONG_HEADER);
-        } catch (CensusAnalyserException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals("Error capturing CSV header!", e.getMessage());
-        }
-    }
 }
 
 
